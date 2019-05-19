227311907b6e166aac3525962b3a86ddc0976147
adrianBarragan <abarragan@navent.com>
2019-05-19 03:43:58 -0300

    complete

--- src/Pages/DetailPage/DetailPage.js
+++ src/Pages/DetailPage/DetailPage.js
@@ -8,7 +8,9 @@ class DetailPage extends Component {
     super(props);
     this.state = {
       perfil: [],
+      categories: [],
       amountPrice: "",
+      decimals: "",
       error: null,
       isLoaded: false
     };
@@ -20,7 +22,9 @@ class DetailPage extends Component {
         console.log(data.item);
         this.setState({
           perfil: data.item,
+          categories: data.categories,
           amountPrice: data.item.price.amount,
+          decimals: data.item.price.decimals,
           isLoaded: true
         });
       });
@@ -35,31 +39,48 @@ class DetailPage extends Component {
   };
 
   render() {
+    if (this.state.decimals < 10) {
+      let decimlasString = "0" + this.state.decimals;
+    }
     return (
-      <div id="producto-detalle">
-        <div className="row" id="breadcrumb">
-          <Breadcrumb data={this.state.category} key={1} />
-        </div>
-        <img
-          className="producto-img"
-          src="https://mla-s1-p.mlstatic.com/752415-MLA25242878719_122016-O.jpg"
-        />
-        <div className="detalle-info">
-          <div className="condicion">{this.state.perfil.condition}</div>
-          <div className="cantidad-vendidos">
-            {this.state.perfil.soldQuantity} vendidos
+      <div className="container">
+        <div className="row">
+          <div className="col-md-10 col-md-offset-1">
+            <div className="row" id="breadcrumb">
+              {this.state.categories.map((category, i) => (
+                <Breadcrumb category={category} key={i} />
+              ))}
+            </div>
+            <div className="row" id="producto-detalle">
+              <img className="producto-img" src={this.state.perfil.picture} />
+              <div className="detalle-info">
+                <div className="condicion">{this.state.perfil.condition} -</div>
+                <div className="cantidad-vendidos">
+                  {this.state.perfil.soldQuantity} vendidos
+                </div>
+                <h1 className="title">{this.state.perfil.title}</h1>
+                <div className="price">
+                  <span>$</span>
+                  <span>{this.state.amountPrice}</span>
+                  {this.state.decimals < 10 && (
+                    <span className="priceDecimals">
+                      0{this.state.decimals}
+                    </span>
+                  )}
+                </div>
+                <button type="button" className="btn btn-primary">
+                  Comprar
+                </button>
+              </div>
+              <h3 className="producto-descripcion-title">
+                Descripción del producto
+              </h3>
+              <div className="producto-descripcion">
+                {this.state.perfil.description}
+              </div>
+            </div>
           </div>
-          <h1 className="title">{this.state.title}</h1>
-          <div className="price">
-            <span>$</span>
-            {this.state.amountPrice}
-          </div>
-          <button type="button" className="btn btn-primary">
-            Comprar
-          </button>
         </div>
-        <h3>Descripción del producto</h3>
-        <div id="producto-descripcion">{this.state.perfil.description}</div>
       </div>
     );
   }
